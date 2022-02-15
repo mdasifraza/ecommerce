506f388a06d7fa2093f2bbd7f3eb2a1d597f1566
mdasifraza <mdasifr28@gmail.com>
2022-02-15 23:59:39 +0530

    order routes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -10,3 +10,26 @@ exports.newOrder = catchAsyncError(async (req, res, next) => {
 
     res.status(200).json({ success: true, order });
 });
+
+// admin only
+exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
+    const order = await Orders.findById(req.params.id).populate("user", "name email");
+    // const order = await Orders.findById(req.params.id);
+
+    if (!order) {
+        return next(new ErrorHandler("Order not found with this id", 404));
+    }
+
+    res.status(200).json({ success: true, order });
+});
+
+//for user specific order details
+exports.myOrders = catchAsyncError(async (req, res, next) => {
+    const order = await Orders.find({ user: req.user._id });
+
+    // if (!order) {
+    //     return next(new ErrorHandler("Order not found with this id", 404));
+    // }
+
+    res.status(200).json({ success: true, order });
+})
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { isAusthenticatedUser, authorizeRoles } = require('../middleware/auth');
-const { newOrder } = require('../controllers/orderController');
+const { newOrder, getSingleOrder, myOrders } = require('../controllers/orderController');
 
 const router = express.Router();
 
 router.route('/order/new').post(isAusthenticatedUser, newOrder);
+router.route('/order/me').get(isAusthenticatedUser, myOrders);
+router.route('/order/:id').get(isAusthenticatedUser, authorizeRoles("admin"), getSingleOrder);
 
 module.exports = router;
