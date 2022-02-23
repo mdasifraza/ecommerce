20cd5e4829189d71c719417bb64455c5c6a8f9b7
mdasifraza <mdasifr28@gmail.com>
2022-02-23 23:49:09 +0530

    product details

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+
 
 const ProductDetails = () => {
   return (
-    <div>ProductDetails</div>
+    <>
+    <div className="ProductDetails"></div>
+    </>
   )
 }
 
