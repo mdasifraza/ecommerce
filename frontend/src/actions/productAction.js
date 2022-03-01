9d20523e3ac30989e11e2ea7977a531db54238b4
mdasifraza <mdasifr28@gmail.com>
2022-03-02 01:01:08 +0530

    product details

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -28,7 +28,7 @@ export const getProduct = () => async (dispatch) => {
 export const getProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_DETAILS_REQUEST });
-        const { data } = await axios.get(`/api/v1/product/${id}`);
+        const { data } = await axios.get(`/api/v1/products/${id}`);
         dispatch({
             type: PRODUCT_DETAILS_SUCCESS,
             payload: data.product,
--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -1,10 +1,83 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Carousel from 'react-material-ui-carousel';
+import { useSelector, useDispatch } from 'react-redux';
+import { getProductDetails } from '../../actions/productAction';
+import { useParams } from 'react-router-dom';
+import ReactStars from 'react-rating-stars-component';
+import './ProductDetails.css';
 
+const ProductDetails = ({ match }) => {
+  const { id } = useParams()
+  console.log(id)
+  const dispatch = useDispatch();
+  const { product, loading, error } = useSelector((state) => state.productDetails);
 
-const ProductDetails = () => {
+  useEffect(() => {
+    dispatch(getProductDetails(id))
+    // dispatch(getProductDetails(match.params.id))
+  }, [dispatch, id]);
+  // }, [dispatch, match.params.id]);
+
+  const options = {
+    edit: false,
+    color: "rgb(20, 20, 20, 0.1)",
+    activeColor: "tomato",
+    size: window.innerWidth < 600 ? 20 : 25,
+    value: product.ratings,
+    isHalf: true,
+  }
+
   return (
     <>
-    <div className="ProductDetails"></div>
+      <div className="ProductDetails">
+        <div>
+          <Carousel>
+            {product.images && product.images.map((item, i) => (
+              <img
+                className="CarouselImage"
+                key={item.url}
+                src={item.url}
+                alt={`${i} Slide`}
+              />
+            ))}
+          </Carousel>
+        </div>
+
+        <div>
+          <div className="detailsBlock-1">
+            <h2>{product.name}</h2>
+            <p>Product # {product._id}</p>
+          </div>
+
+          <div className="detailsBlock-2">
+            <ReactStars {...options} />
+            <span>({product.numOfReviews}Reviews)</span>
+          </div>
+
+          <div className="detailsBlock-3">
+            <h1>{`₹${product.price}`}</h1>
+            <div className="detailsBlock-3-1">
+              <div className="detailsBlock-3-1-1">
+                <button>-</button>
+                <input type="number" value="1" />
+                <button>+</button>
+              </div>
+              <button>Add to Cart</button>
+            </div>
+            <p>
+              Status: <b className={product.Stock < 1 ? "redColor" : "greenColor"} >
+                {product.Stock < 1 ? "OutOfStock" : "InStock"}
+              </b>
+            </p>
+          </div>
+
+          <div className="detailsBlock-4">
+            Description : <p>{product.description}</p>
+          </div>
+
+          <button className="submitReview" >Submit Review</button>
+        </div>
+      </div>
     </>
   )
 }
